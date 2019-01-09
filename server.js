a3a3ba752a605375cc78a84894647093217a419c
HasnaaMejdoub <mejdoub.hasnaa@gmail.com>
2019-01-09 16:30:04 +0100

    updates to deploy to heroku 3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('/dist/test-demo'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/test-demo/index.html'));
+res.sendFile(path.join('/dist/test-demo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
